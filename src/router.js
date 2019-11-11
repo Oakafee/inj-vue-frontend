2cfec0bb9941ba43f2c01fece2b17e55e6f6a113
Jesse Fried <jefrttt@gmail.com>
2019-11-11 12:52:39 -0500

    edit mode goes away on route update

--- src/router.js
+++ src/router.js
@@ -34,9 +34,13 @@ router.beforeEach((to, from, next) => {
 	//TODO: improve this second condition
 		store.commit('saveDialogIntervention', to);
 		changeRoute = false;
-	}
-	if (changeRoute && store.state.articleMapExpanded) {
-		store.commit('toggleArticleMapSize', false);
+	} else {
+		if (store.state.articleMapExpanded) {
+			store.commit('toggleArticleMapSize', false);
+		}
+		if (store.state.editableArticle) {
+			store.commit('editArticle', null);
+		}
 	}
 	next(changeRoute);
 });
